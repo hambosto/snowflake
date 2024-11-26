f4394982ad2146c561d7de95a4df34a440ffd794
Ilham Putra Husada <hambosto@gmail.com>
2024-11-26 22:57:12 +0700

    refactor(nixos): simplify module imports

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
           inherit hostname;
         };
         modules = [
-          ./hosts/${hostname}/configuration.nix
+          ./hosts/configuration.nix
           inputs.stylix.nixosModules.stylix
           home-manager.nixosModules.home-manager
           {
@@ -48,7 +48,7 @@
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.backupFileExtension = "hm-backup";
-            home-manager.users.${username} = import ./hosts/${hostname}/home.nix;
+            home-manager.users.${username} = import ./hosts/home.nix;
           }
         ];
       };
