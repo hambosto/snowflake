8284a97ec48e51f0017f8eab60f4c60f1f1b7b18
Ilham Putra Husada <hambosto@gmail.com>
2024-11-28 18:06:07 +0700

    fix(services): disable udisks2 auto mounting

--- modules/services/core.nix
+++ modules/services/core.nix
@@ -1,13 +1,11 @@
 {
   services = {
-    libinput.enable = true;
-    acpid.enable = true;
     upower.enable = true;
     power-profiles-daemon.enable = true;
 
     dbus.enable = true;
     gvfs.enable = true;
     fstrim.enable = true;
-    udisks2.enable = true;
+    udisks2.enable = false; # usb auto mounting
   };
 }
