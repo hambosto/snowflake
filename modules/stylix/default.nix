1e7cf875d4109923bdc3cd342dfb04802b492430
Ilham Putra Husada <hambosto@gmail.com>
2024-11-30 00:02:25 +0700

    build: update dependencies and remove comments

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -8,22 +8,22 @@
     enable = true;
 
     base16Scheme = {
-      base00 = "0a0b1a"; # Default Background - Deep Space Black
-      base01 = "141526"; # Lighter Background - Midnight Core
-      base02 = "1e1f35"; # Selection Background - Deep Navy
-      base03 = "2b2d4d"; # Comments - Twilight Purple
-      base04 = "4d4f7f"; # Dark Foreground - Mystic Violet
-      base05 = "f8e2ff"; # Default Foreground - Cotton Candy White
-      base06 = "ffd6f3"; # Light Foreground - Bubble Gum Pink
-      base07 = "ffebf9"; # Light Background - Sweet Pink
-      base08 = "ff3c7c"; # Variables - Hot Pink
-      base09 = "ff5d4d"; # Integers - Cyber Red
-      base0A = "ffcc00"; # Classes - Electric Yellow
-      base0B = "00ff9f"; # Strings - Neon Green
-      base0C = "00ffff"; # Support - Cyber Cyan
-      base0D = "7c4dff"; # Functions - Deep Purple
-      base0E = "f54fff"; # Keywords - Sugar Purple
-      base0F = "ff6e9c"; # Deprecated - Strawberry Pink
+      base00 = "0a0b1a";
+      base01 = "141526";
+      base02 = "1e1f35";
+      base03 = "2b2d4d";
+      base04 = "4d4f7f";
+      base05 = "f8e2ff";
+      base06 = "ffd6f3";
+      base07 = "ffebf9";
+      base08 = "ff3c7c";
+      base09 = "ff5d4d";
+      base0A = "ffcc00";
+      base0B = "00ff9f";
+      base0C = "00ffff";
+      base0D = "7c4dff";
+      base0E = "f54fff";
+      base0F = "ff6e9c";
     };
 
     polarity = "dark";
