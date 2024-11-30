9c4f248fbe0fd27978eed98e5273dcaf477013c6
Ilham Putra Husada <hambosto@gmail.com>
2024-11-30 23:17:57 +0700

    chore(pipewire): remove rnnoise config

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,48 +1,8 @@
-{ pkgs, ... }:
-# let
-#   rnnoise_config = {
-#     "context.modules" = [
-#       {
-#         "name" = "libpipewire-module-filter-chain";
-#         "args" = {
-#           "node.description" = "Noise Canceling Source";
-#           "media.name" = "Noise Canceling Source";
-#           "filter.graph" = {
-#             "nodes" = [
-#               {
-#                 "type" = "ladspa";
-#                 "name" = "rnnoise";
-#                 "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
-#                 "label" = "noise_suppressor_stereo";
-#                 "control" = {
-#                   "VAD Threshold (%)" = 50.0;
-#                 };
-#               }
-#             ];
-#           };
-#           "audio.position" = [
-#             "FL"
-#             "FR"
-#           ];
-#           "capture.props" = {
-#             "node.name" = "effect_input.rnnoise";
-#             "node.passive" = true;
-#           };
-#           "playback.props" = {
-#             "node.name" = "effect_output.rnnoise";
-#             "media.class" = "Audio/Source";
-#           };
-#         };
-#       }
-#     ];
-#   };
-# in
 {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # extraConfig.pipewire."99-input-denoising" = rnnoise_config;
   };
 }
