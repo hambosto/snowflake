9df1cbc4595539b43275461ae7d289377dabfa72
Ilham Putra Husada <hambosto@gmail.com>
2024-11-30 22:55:37 +0700

    chore(pipewire): remove camera disable config, prepare rnnoise

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -1,19 +1,48 @@
+{ pkgs, ... }:
+# let
+#   rnnoise_config = {
+#     "context.modules" = [
+#       {
+#         "name" = "libpipewire-module-filter-chain";
+#         "args" = {
+#           "node.description" = "Noise Canceling Source";
+#           "media.name" = "Noise Canceling Source";
+#           "filter.graph" = {
+#             "nodes" = [
+#               {
+#                 "type" = "ladspa";
+#                 "name" = "rnnoise";
+#                 "plugin" = "${pkgs.rnnoise-plugin}/lib/ladspa/librnnoise_ladspa.so";
+#                 "label" = "noise_suppressor_stereo";
+#                 "control" = {
+#                   "VAD Threshold (%)" = 50.0;
+#                 };
+#               }
+#             ];
+#           };
+#           "audio.position" = [
+#             "FL"
+#             "FR"
+#           ];
+#           "capture.props" = {
+#             "node.name" = "effect_input.rnnoise";
+#             "node.passive" = true;
+#           };
+#           "playback.props" = {
+#             "node.name" = "effect_output.rnnoise";
+#             "media.class" = "Audio/Source";
+#           };
+#         };
+#       }
+#     ];
+#   };
+# in
 {
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
-    wireplumber = {
-      enable = true;
-      extraConfig = {
-        "10-disable-camera" = {
-          "wireplumber.profiles" = {
-            main."monitor.libcamera" = "disabled";
-          };
-        };
-      };
-    };
+    # extraConfig.pipewire."99-input-denoising" = rnnoise_config;
   };
 }
