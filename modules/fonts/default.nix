f3f76f335c6f0d35997a21fb6987231016a74434
Ilham Putra Husada <hambosto@gmail.com>
2024-12-05 16:58:40 +0700

    feat(fonts): enable font hinting and antialiasing

--- modules/fonts/default.nix
+++ modules/fonts/default.nix
@@ -9,6 +9,12 @@
     enableDefaultPackages = false;
 
     fontconfig = {
+      antialias = true;
+      cache32Bit = true;
+      hinting = {
+        enable = true;
+        autohint = true;
+      };
       defaultFonts = {
         monospace = [ "JetBrainsMono Nerd Font" ];
         sansSerif = [ "Noto Sans" ];
