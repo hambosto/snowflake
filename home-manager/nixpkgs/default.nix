f83d30de10f01b6c4089cc70fa0331512d9e851a
Ilham Putra Husada <hambosto@gmail.com>
2024-12-09 23:14:13 +0700

    chore(nixpkgs): add nur overlay

--- home-manager/nixpkgs/default.nix
+++ home-manager/nixpkgs/default.nix
@@ -4,6 +4,7 @@
     overlays = [
       inputs.hyprpanel.overlay
       inputs.nix-vscode-extensions.overlays.default
+      inputs.nur.overlays.default
     ];
     config = {
       allowBroken = true;
