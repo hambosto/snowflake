9db943c2fca4c748bb89dabb3de78de6dc8fe9fc
Ilham Putra Husada <hambosto@gmail.com>
2024-12-11 13:57:27 +0700

    chore(go-encryption): update to v0.0.3 and enable static build

--- home-manager/programs/go-encryption.nix
+++ home-manager/programs/go-encryption.nix
@@ -12,17 +12,22 @@ in
     home.packages = [
       (pkgs.buildGoModule rec {
         pname = "go-encryption";
-        version = "0.0.2";
+        version = "0.0.3";
         src = pkgs.fetchFromGitHub {
           owner = "hambosto";
           repo = "go-encryption";
           rev = "v${version}";
-          hash = "sha256-00GNQmPkJOvZ38ZOAXhT5hAZoIGBt6NtL1RDDbJqPns=";
+          hash = "sha256-A8OvDJ9/BsnEpxss97segCLBDBJOo9Q1hy5Tk1UKe3o=";
         };
         vendorHash = "sha256-6fKolW8dBS0R6RvKfaOPwljRRZruVN+ATi4qEkk3VXY=";
+
+        CGO_ENABLED = 0;
+
         ldflags = [
+          "-extldflags '-static'"
           "-s -w"
         ];
+
         meta = with lib; {
           description = "Secure file encryption and decryption CLI tool built with Go";
           homepage = "https://github.com/hambosto/go-encryption";
