654d339f6451fd8b5983dbd87880697a3adcf853
Ilham Putra Husada <hambosto@gmail.com>
2024-12-28 13:33:48 +0700

    style(theme): update base16 colorscheme

--- modules/stylix/default.nix
+++ modules/stylix/default.nix
@@ -8,22 +8,22 @@
     enable = true;
 
     base16Scheme = {
-      base00 = "0a0b1a";
-      base01 = "141526";
-      base02 = "1e1f35";
-      base03 = "2b2d4d";
-      base04 = "4d4f7f";
-      base05 = "f8e2ff";
-      base06 = "ffd6f3";
-      base07 = "ffebf9";
-      base08 = "ff3c7c";
-      base09 = "ff5d4d";
-      base0A = "ffcc00";
-      base0B = "00ff9f";
-      base0C = "00ffff";
-      base0D = "7c4dff";
-      base0E = "f54fff";
-      base0F = "ff6e9c";
+      base00 = "1e1e2e"; # base
+      base01 = "181825"; # mantle
+      base02 = "313244"; # surface0
+      base03 = "45475a"; # surface1
+      base04 = "585b70"; # surface2
+      base05 = "cdd6f4"; # text
+      base06 = "f5e0dc"; # rosewater
+      base07 = "b4befe"; # lavender
+      base08 = "f38ba8"; # red
+      base09 = "fab387"; # peach
+      base0A = "f9e2af"; # yellow
+      base0B = "a6e3a1"; # green
+      base0C = "94e2d5"; # teal
+      base0D = "89b4fa"; # blue
+      base0E = "cba6f7"; # mauve
+      base0F = "f2cdcd"; # flamingo
     };
 
     polarity = "dark";
