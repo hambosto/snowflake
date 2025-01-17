5753018f9027b43508c9a08793a1cb6422414690
Ilham Putra Husada <hambosto@gmail.com>
2025-01-17 13:09:22 +0700

    chore(hyprland): update window animations

--- home-manager/wayland/hyprland.nix
+++ home-manager/wayland/hyprland.nix
@@ -82,20 +82,21 @@ in
         animations = {
           enabled = true;
           bezier = [
-            "cosmic, 0.25, 0.1, 0.25, 1"
-            "hyperfast, 0.16, 1, 0.3, 1"
-            "hyperdynamic, 0.68, -0.55, 0.27, 1.55"
-            "ultrasmooth, 0.45, 0, 0.55, 1"
-            "workspace, 0.15, 0.9, 0.1, 1.1"
+            "wind, 0.05, 0.9, 0.1, 1.05"
+            "winIn, 0.1, 1.1, 0.1, 1.1"
+            "winOut, 0.3, -0.3, 0, 1"
+            "liner, 1, 1, 1, 1"
           ];
           animation = [
-            "windows, 1, 7, cosmic, popin 30%"
-            "windowsIn, 1, 5, hyperfast, slide"
-            "windowsOut, 1, 7, hyperdynamic, slidevert"
-            "border, 1, 10, default"
-            "borderangle, 1, 50, ultrasmooth, once"
-            "fade, 1, 5, cosmic"
-            "workspaces, 1, 3, workspace, slide"
+            "windows, 1, 6, wind, slide"
+            "windowsIn, 1, 6, winIn, slide"
+            "windowsOut, 1, 5, winOut, slide"
+            "windowsMove, 1, 5, wind, slide"
+            "border, 1, 1, liner"
+            "borderangle, 1, 30, liner, once"
+            "fade, 1, 10, default"
+            "workspaces, 1, 5, wind"
+            "specialWorkspace, 1, 5, wind, slidevert"
           ];
         };
 
