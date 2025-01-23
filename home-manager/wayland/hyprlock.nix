b841ece857dee452e8f682832e2e6fe8bd3c174b
Ilham Putra Husada <hambosto@gmail.com>
2025-01-23 19:51:22 +0700

    chore(hyprland): update hyprlock config

--- home-manager/wayland/hyprlock.nix
+++ home-manager/wayland/hyprlock.nix
@@ -11,12 +11,12 @@ in
     stylix.targets.hyprlock.enable = true;
     programs.hyprlock = {
       enable = true;
+
       settings = {
         general = {
-          grace = 5;
-          no_fade_in = false;
-          disable_loading_bar = true;
+          ignore_empty_input = true;
         };
+
         background = {
           monitor = "";
           blur_passes = 3;
@@ -25,56 +25,83 @@ in
           vibrancy = 0.1696;
           vibrancy_darkness = 0.0;
         };
+
         input-field = lib.mkForce {
           monitor = "";
-          size = "300, 60";
-          outline_thickness = 2;
-          dots_size = 0.2;
-          dots_spacing = 0.2;
+          size = "200, 50";
+          outline_thickness = 3;
+          dots_size = 0.33;
+          dots_spacing = 0.15;
           dots_center = true;
-          outer_color = "rgba(0, 0, 0, 0)";
-          inner_color = "rgba(255, 255, 255, 0.1)";
-          font_color = "rgb(200, 200, 200)";
-          fade_on_empty = false;
-          font_family = config.stylix.fonts.sansSerif.name + "Bold";
-          placeholder_text = "<i>🔒 Enter Password...</i>";
+          dots_rounding = -1;
+          outer_color = "rgb(151515)";
+          inner_color = "rgb(FFFFFF)";
+          fade_on_empty = true;
+          fade_timeout = 1000;
+          placeholder_text = "<i>Input Password...</i>";
           hide_input = false;
-          position = "0, -290";
+          rounding = 40;
+          check_color = "rgb(204, 136, 34)";
+          fail_color = "rgb(204, 34, 34)";
+          fail_text = "<i>$FAIL <b>($ATTEMPTS)</b></i>";
+          fail_transition = 300;
+          capslock_color = -1;
+          numlock_color = -1;
+          bothlock_color = -1;
+          invert_numlock = false;
+          swap_font_color = false;
+          position = "0, -20";
           halign = "center";
           valign = "center";
+          shadow_passes = 10;
+          shadow_size = 20;
+          shadow_color = "rgb(0,0,0)";
+          shadow_boost = 1.6;
         };
+
         label = [
           {
             monitor = "";
-            text = ''cmd[update:1000] echo -e "$(date +"%A, %d %B %Y")"'';
-            color = "#${config.lib.stylix.colors.base05}";
-            font_size = 24;
-            font_family = config.stylix.fonts.sansSerif.name + " Regular";
-            position = "0, 500";
-            halign = "center";
-            valign = "center";
+            text = "cmd[update:1000] echo \"$TIME\"";
+            color = "rgba(200, 200, 200, 1.0)";
+            font_size = 55;
+            font_family = "Fira Semibold";
+            position = "-100, 70";
+            halign = "right";
+            valign = "bottom";
+            shadow_passes = 5;
+            shadow_size = 10;
           }
           {
             monitor = "";
-            text = ''cmd[update:1000] echo "<b>$(date +"%H:%M:%S")</b>"'';
-            color = "#${config.lib.stylix.colors.base05}";
-            font_size = 120;
-            font_family = config.stylix.fonts.sansSerif.name + " Bold";
-            position = "0, 350";
-            halign = "center";
-            valign = "center";
-          }
-          {
-            monitor = "";
-            text = "Hello, ${config.settings.system.fullName}!";
-            color = "#${config.lib.stylix.colors.base0A}";
+            text = "$USER";
+            color = "rgba(200, 200, 200, 1.0)";
             font_size = 20;
-            font_family = config.stylix.fonts.sansSerif.name;
-            position = "0, 150";
-            halign = "center";
-            valign = "center";
+            font_family = "Fira Semibold";
+            position = "-100, 160";
+            halign = "right";
+            valign = "bottom";
+            shadow_passes = 5;
+            shadow_size = 10;
           }
         ];
+
+        image = {
+          monitor = "";
+          size = 280;
+          rounding = 40;
+          border_size = 4;
+          border_color = "rgb(221, 221, 221)";
+          rotate = 0;
+          reload_time = -1;
+          position = "0, 200";
+          halign = "center";
+          valign = "center";
+          shadow_passes = 10;
+          shadow_size = 20;
+          shadow_color = "rgb(0,0,0)";
+          shadow_boost = 1.6;
+        };
       };
     };
   };
