0db4e8eb6fa79f955af67d57aa164cd3e69f67f0
Ilham Putra Husada <hambosto@gmail.com>
2025-02-08 16:29:33 +0700

    chore(hyprland): enable workspace icons

--- home-manager/wayland/hyprpanel.nix
+++ home-manager/wayland/hyprpanel.nix
@@ -66,10 +66,10 @@ in
         bar.workspaces.show_numbered = false;
         bar.workspaces.workspaces = 5;
         bar.workspaces.monitorSpecific = false;
-        # bar.workspaces.numbered_active_indicator = "color";
-        # bar.workspaces.applicationIconEmptyWorkspace = "";
-        # bar.workspaces.showApplicationIcons = true;
-        # bar.workspaces.showWsIcons = true;
+        bar.workspaces.numbered_active_indicator = "color";
+        bar.workspaces.applicationIconEmptyWorkspace = "";
+        bar.workspaces.showApplicationIcons = true;
+        bar.workspaces.showWsIcons = true;
         bar.windowtitle.label = true;
         bar.volume.label = true;
         bar.network.showWifiInfo = false;
