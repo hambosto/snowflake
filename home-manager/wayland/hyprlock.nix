787f50c8b757fe2489b67d16e39af821e9d4fc63
Ilham Putra Husada <hambosto@gmail.com>
2025-02-25 13:39:51 +0700

    chore(hyprland): update hyprlock configuration

--- home-manager/wayland/hyprlock.nix
+++ home-manager/wayland/hyprlock.nix
@@ -8,17 +8,16 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    stylix.targets.hyprlock.enable = true;
     programs.hyprlock = {
       enable = true;
 
       settings = {
         general = {
-          ignore_empty_input = true;
-          grace = 0;
+          hide_cursor = true;
+          grace = 2;
         };
 
-        background = {
+        background = lib.mkForce {
           blur_passes = 3;
           blur_size = 10;
           brightness = 1.0;
@@ -26,45 +25,25 @@ in
           noise = 0.02;
         };
 
-        input-field = lib.mkForce {
-          monitor = "";
-          size = "250, 50";
-          outline_thickness = 0;
-          dots_size = 0.26;
-          inner_color = "#${config.lib.stylix.colors.base05}";
-          dots_spacing = 0.64;
-          dots_center = true;
-          fade_on_empty = true;
-          placeholder_text = "<i>Password...</i>";
-          hide_input = false;
-          check_color = "rgb(40, 200, 250)";
-          position = "0, 50";
+        label = {
+          text = "パスワードをご入力ください";
+          color = "rgba(222, 222, 222, 1.0)";
+          font_size = 50;
+          font_family = "Noto Sans CJK JP";
+          position = "0, 70";
           halign = "center";
-          valign = "bottom";
+          valign = "center";
         };
 
-        label = [
-          {
-            monitor = "";
-            text = "cmd[update:1000] echo \"<b><big> $(date +\"%H:%M\") </big></b>\"";
-            color = "#${config.lib.stylix.colors.base05}";
-            font_size = 64;
-            font_family = "JetBrains Mono Nerd Font 10";
-            position = "0, -70";
-            halign = "center";
-            valign = "center";
-          }
-          {
-            monitor = "";
-            text = "cmd[update:18000000] echo \"<b> \"$(date +\'%A, %-d %B %Y\')\"\</b>\"";
-            color = "#${config.lib.stylix.colors.base05}";
-            font_size = 24;
-            font_family = "JetBrains Mono Nerd Font 10";
-            position = "0, -120";
-            halign = "center";
-            valign = "center";
-          }
-        ];
+        input-field = {
+          size = "50, 50";
+          dots_size = 0.33;
+          dots_spacing = 0.15;
+          outer_color = lib.mkForce "rgba(25, 20, 20, 0)";
+          inner_color = lib.mkForce "rgba(25, 20, 20, 0)";
+          font_color = lib.mkForce "rgba(222, 222, 222, 1.0)";
+          placeholder_text = "パスワード";
+        };
       };
     };
   };
