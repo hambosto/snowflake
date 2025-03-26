1363c161b8bca02040fb65a5acf720a1c429f770
Ilham Putra Husada <hambosto@gmail.com>
2025-03-27 01:24:51 +0700

    chore(flake): update flake lockfile

--- home-manager/programs/zed-editor.nix
+++ home-manager/programs/zed-editor.nix
@@ -1,26 +1,26 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   programs.zed-editor = {
     enable = false;
+    installRemoteServer = true;
+    extraPackages = [ pkgs.nixd ];
     userSettings = {
       theme = lib.mkForce "Tokyo Night";
       ui_font_size = lib.mkForce 18;
       ui_font_family = lib.mkForce "JetBrainsMono Nerd Font";
       buffer_font_size = lib.mkForce 14;
       buffer_font_family = lib.mkForce "JetBrainsMono Nerd Font";
+      inlay_hints.enabled = true;
 
       telemetry = {
         diagnostics = false;
         metrics = false;
       };
     };
+
     extensions = [
       "tokyo-night"
-      "dockerfile"
-      "docker-compose"
-      "sql"
-      "make"
+      "nix"
     ];
-
   };
 }
